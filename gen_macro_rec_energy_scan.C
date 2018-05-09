bd3891ce902f2e6b4b5f3427f0f7fe379e541ae1
Jongsik Eum <jongsikeum@me.com>
2018-05-09 12:59:30 +0900

    Init: Macro generator for energy scan.

---
+++ gen_macro_rec_energy_scan.C
@@ -0,0 +1,109 @@
+// GUI input
+// 0: No event display
+// 1: OGS
+// 2: HEPREP
+// 3: DAWN (Not yet added in this code)
+void gen_macro_rec(int gui =0, int runAcc = 0){ 
+
+	std::ofstream ofs ("run_generated.mac", std::ofstream::out);
+
+	int const stepEnergy = 6;
+	float momentum[stepEnergy] = {194, 190, 185, 179, 174, 168}; //Unit: MeV
+
+
+
+	int event_map1[10][10] = {  	
+		{0,	0,	0,	0,	0,	0,	0,	0,	0,	0},
+		{0,	0,	0,	0,	0,	0,	0,	0,	0,	0},
+		{0,	0,	0,	0,	0,	0,	0,	0,	0,	0},
+		{0,	0,	0,	0,	0,	0,	0,	0,	0,	0},
+		{0,	0,	0,	4,	4,	1,	0,	0,	0,	0},
+		{0,	0,	0,	5,	4,	5,	0,	0,	0,	0},
+		{0,	0,	0,	0,	2,	0,	0,	0,	0,	0},
+		{0,	0,	0,	0,	0,	0,	0,	0,	0,	0},
+		{0,	0,	0,	0,	0,	0,	0,	0,	0,	0},
+		{0,	0,	0,	0,	0,	0,	0,	0,	0,	0}
+	};	
+
+	ofs << "# KOMAC Simulator" << endl;
+	ofs << "#" << endl;
+	if (gui==1){
+		ofs << "# Add trajectories to the visualization." << endl;
+		ofs << "/vis/scene/add/trajectories" << endl;
+		ofs << "# Accumulate multiple events in one picture." << endl;
+		ofs << "/vis/scene/endOfEventAction accumulate -1" << endl;
+		ofs << "# Multithread mode" << endl;
+		ofs << "/vis/multithreading/maxEventQueueSize -1" << endl;
+		ofs << "/vis/ogl/set/displayListLimit 9999999" << endl;
+		ofs << "#Multithreading mode for visulaization" << endl;
+		ofs << "/vis/multithreading/maxEventQueueSize -1" << endl;
+		ofs << "/vis/multithreading/actionOnEventQueueFull wait" << endl;
+	}
+
+	if (gui==2){
+		ofs << "# HepRepFile diriver" << endl;
+		ofs << "/vis/open HepRepFile" << endl;
+		ofs << "# Add world volume" << endl;
+		ofs << "/vis/drawVolume" << endl;
+		ofs << "# Add trajectories to the visualization." << endl;
+		ofs << "/vis/scene/add/trajectories" << endl;
+		ofs << "# Accumulate multiple events in one picture." << endl;
+		ofs << "/vis/scene/endOfEventAction accumulate -1" << endl;
+		// ofs << "/vis/drawOnlyToBeKeptEvents" << endl;
+		ofs << "#Multithreading mode for visulaization" << endl;
+		ofs << "/vis/multithreading/maxEventQueueSize -1" << endl;
+		ofs << "/vis/multithreading/actionOnEventQueueFull wait" << endl;
+	}
+
+	if (runAcc>0){
+		ofs << "# Accumulate multiple runs in one picture." << endl;
+		ofs << "/vis/scene/endOfRunAction accumulate" << endl;  	
+	}
+
+	// ofs << "# Initialize kernel" << endl;
+	// ofs << "/run/initialize" << endl;
+	// ofs << "# Particle Energy" << endl;
+	// ofs << Form("/tutorial/generator/momentum %f MeV", momentum) << endl;
+
+	int maxBin = 10;
+	float start_xBin_position = -100;
+	float start_yBin_position = -100;
+
+	for (int iEnergy = 0; iEnergy < stepEnergy; ++iEnergy)
+	{
+		ofs << "# Initialize kernel" << endl;
+		ofs << "/run/initialize" << endl;
+		ofs << "# Particle Energy" << endl;
+		ofs << Form("/tutorial/generator/momentum %f MeV", momentum[iEnergy]) << endl;
+		for (int i = 0; i < 1; ++i)
+		{
+			for (int iX = 0; iX < maxBin; ++iX)
+			{
+				for (int iY = 0; iY < maxBin; ++iY){
+					if(event_map1[9-iY][iX]==0) continue;
+					ofs << "################################################" << endl;
+					ofs << "#" << endl;
+					ofs << Form("# cell coordinate: %d,%d", iX+1, iY+1) << endl;
+					ofs << "#" << endl;
+					ofs << "# Particle position distribution in polar angle" << endl;
+					ofs << Form("/tutorial/generator/PositionX %f mm", start_xBin_position + 20*iX) << endl;
+					ofs << Form("/tutorial/generator/PositionY %f mm", start_yBin_position + 20*iY) << endl;
+					ofs << "#" << endl;
+					ofs << "# Rootfile setup" << endl;
+					ofs << "#" << endl;
+					ofs << Form("/analysis/setFileName run%d_%d_%d_e_%d", i+1, iX, iY, 20-iEnergy) << endl;
+					ofs << "/analysis/h1/set 1 100 1 30 MeV" << endl;
+					ofs << "/analysis/h2/setX 1 300 -15 15 cm  #x-y" << endl;
+					ofs << "/analysis/h2/setY 1 300 -15 15 cm  #x-y" << endl;
+					ofs << "/analysis/h2/setX 4 120 0 360 deg" << endl;
+					ofs << "/analysis/h2/setY 4 101 -1.005 1.005 none" << endl;
+					ofs << "#" << endl;
+					ofs << "/run/printProgress 100" << endl;
+					ofs << "#" << endl;
+					ofs << Form("/run/beamOn %d", 500*event_map1[9-iY][iX]) << endl;  		
+				}
+			}
+		}
+	}
+	ofs.close();
+}
